23426bdfd8d14b836c7d9329fa3fa44effd8b01f
vaishnavi <vaishnavi.v@perfios.com>
2023-07-08 23:51:31 +0530

    "ADDED : Expenses Date code with css."

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString("en-IN", {month : "long"});
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString("en-IN", {day: '2-digit'});
-
+    
     return (
     <div className = 'expense-item'>
+        <ExpenseDate date= {props.date}></ExpenseDate>
         {/* <div>{expenseDate.toISOString()}</div> */}
-        <div>
         {/* <div>{props.date.toLocaleString("en-IN", {month : "long"})}</div> */}
-        <div className="expende-date">{day}</div>
-        <div className="expende-date__month">{year}</div>
-        <div className="expende-date__year">{month}</div>
-        </div>
 
         <div className = 'expense-item__description'>
             {/* <h2>{expenseTitle}</h2> */}
