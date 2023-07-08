590eed4ca63acf0a727df8394279b0168b282170
vaishnavi <vaishnavi.v@perfios.com>
2023-07-08 21:14:27 +0530

    code for refactoring date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,27 @@
 import './ExpenseItem.css'
-// function ExpenseItem(props) {
-    // object destructuring {date, amount, title}
-function ExpenseItem({date, amount, title}) {
 
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString("en-IN", {month : "long"});
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString("en-IN", {day: '2-digit'});
 
-    const expenseDate = new Date(2023, 5, 7);
-    const expenseTitle = 'Bag';
-    const expenseAmount = 55500;
-
-    return <div className = 'expense-item'>
+    return (
+    <div className = 'expense-item'>
         {/* <div>{expenseDate.toISOString()}</div> */}
-        {/* <div>{props.date.toISOString()}</div> */}
-        <div>{date.toISOString()}</div>
-
+        <div>
+        {/* <div>{props.date.toLocaleString("en-IN", {month : "long"})}</div> */}
+        <div className="expende-date">{day}</div>
+        <div className="expende-date__month">{year}</div>
+        <div className="expende-date__year">{month}</div>
+        </div>
 
         <div className = 'expense-item__description'>
             {/* <h2>{expenseTitle}</h2> */}
-            {/* <h2>{props.title}</h2> */}
-            <h2>{title}</h2>
-
-
+            <h2>{props.title}</h2>
         </div>
         {/* <div className = 'expense-item__price'> $ {expenseAmount}</div> */}
-        {/* <div className = 'expense-item__price'> $ {props.amount}</div> */}
-        <div className = 'expense-item__price'> $ {amount}</div>
-
-
+        <div className = 'expense-item__price'> $ {props.amount}</div>
     </div>
+    );
 }
 export default ExpenseItem;
